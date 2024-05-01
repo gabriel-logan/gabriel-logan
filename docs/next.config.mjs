898f9b556a5c0c735e83b08908ab5d66e50946aa
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-01 14:52:16 -0300

    chore: update folder structure

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
-	distDir: process.env.NODE_ENV === "production" ? "../docs" : undefined,
 	assetPrefix:
 		process.env.NODE_ENV === "production" ? "/gabriel-logan" : undefined,
 	basePath:
