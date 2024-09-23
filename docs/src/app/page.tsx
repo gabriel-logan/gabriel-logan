09f78b32b776650121a7582d276c7f7a451e8b19
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-09-23 01:03:11 -0300

    update portfolio, TEMP solution

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -63,14 +63,24 @@ export default function Home() {
 								</h1>
 							</header>
 							<p>
-								And this is <strong>Miniport</strong>, loren ipsun dolor amet
-								sit loren ipsun dolor amet sit loren ipsun dolor amet sit
+								Full Stack Developer with over 2 years of experience in
+								freelance and open-source projects. I have advanced knowledge of
+								JavaScript, TypeScript and frameworks such as ReactJS, React
+								Native and ExpressJS. I work on both the frontend and backend,
+								creating complete, efficient and scalable solutions, with
+								experience in NestJS, MySQL, AWS, Docker and microservices
+								architecture.
 							</p>
 							<Link href="#work" className="button large scrolly">
 								Learn about what I do
 							</Link>
 						</div>
-						<p>Under development...</p>
+						<Link
+							href="/full-stack-developer-resume-gabriellogan.pdf"
+							className="w-full text-center text-xl"
+						>
+							See my resume
+						</Link>
 					</div>
 				</div>
 			</article>
@@ -80,42 +90,47 @@ export default function Home() {
 				<div className="container">
 					<header>
 						<h2>Here&apos;s all the stuff I do.</h2>
-						<p>Odio turpis amet sed consequat eget posuere consequat.</p>
+						<p>
+							Ornare nulla proin odio consequat sapien vestibulum ipsum sed
+							lorem.
+						</p>
 					</header>
 					<div className="row aln-center">
 						<div className="col-4 col-6-medium col-12-small">
 							<section className="box style1">
 								<span className="icon featured fa-comments" />
-								<h3>Consequat lorem</h3>
+								<h3>Frontend Web Applications</h3>
 								<p>
-									Ornare nulla proin odio consequat sapien vestibulum ipsum
-									primis sed amet consequat lorem dolore.
+									Frontend application development using ReactJs, Typescript,
+									Nextjs and other technologies.
 								</p>
 							</section>
 						</div>
 						<div className="col-4 col-6-medium col-12-small">
 							<section className="box style1">
 								<span className="icon solid featured fa-camera-retro" />
-								<h3>Lorem dolor tempus</h3>
+								<h3>Backend Web Applications</h3>
 								<p>
-									Ornare nulla proin odio consequat sapien vestibulum ipsum
-									primis sed amet consequat lorem dolore.
+									Backend application development using NodeJs, ExpressJs,
+									NestJs, MySQL, MongoDB and other technologies.
 								</p>
 							</section>
 						</div>
 						<div className="col-4 col-6-medium col-12-small">
 							<section className="box style1">
 								<span className="icon featured fa-thumbs-up" />
-								<h3>Feugiat posuere</h3>
+								<h3>Mobile Applications</h3>
 								<p>
-									Ornare nulla proin odio consequat sapien vestibulum ipsum
-									primis sed amet consequat lorem dolore.
+									Mobile application development using React Native, Typescript
+									and other technologies.
 								</p>
 							</section>
 						</div>
 					</div>
 					<footer>
-						<p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+						<p>
+							Wanna see more of my work? Check out some of my projects below.
+						</p>
 						<Link href="#portfolio" className="button large scrolly">
 							See some of my recent work
 						</Link>
@@ -202,7 +217,9 @@ export default function Home() {
 						</div>
 					</div>
 					<footer>
-						<p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+						<p>
+							Wanna see more of my work? Check out my GitHub profile for more
+						</p>
 						<Link href="#contact" className="button large scrolly">
 							Get in touch with me
 						</Link>
@@ -215,7 +232,11 @@ export default function Home() {
 				<div className="medium container">
 					<header>
 						<h2>Have me make stuff for you.</h2>
-						<p>Ornare nulla proin odio consequat sapien vestibulum ipsum.</p>
+						<p>
+							If you like my work and have a project you need help with, think
+							you need my help with something or just fancy saying hey, then get
+							in touch. I&apos;ll be happy to help you.
+						</p>
 					</header>
 					<div className="row">
 						{/**
@@ -277,7 +298,10 @@ export default function Home() {
 							<h3>Find me on ...</h3>
 							<ul className="social">
 								<li>
-									<Link href="#" className="icon brands fa-google-plus">
+									<Link
+										href="https://play.google.com/store/apps/dev?id=5118283462925344777"
+										className="icon brands fa-google-plus"
+									>
 										<span className="label">Google+</span>
 									</Link>
 								</li>
@@ -295,11 +319,6 @@ export default function Home() {
 										<span className="label">YouTube</span>
 									</Link>
 								</li>
-								<li>
-									<Link href="#" className="icon brands fa-vimeo">
-										<span className="label">Vimeo</span>
-									</Link>
-								</li>
 								{/*-->*/}
 							</ul>
 							<hr />
@@ -307,7 +326,7 @@ export default function Home() {
 					</div>
 					<footer>
 						<ul id="copyright">
-							<li>&copy; Untitled. All rights reserved.</li>
+							<li>&copy; All rights reserved.</li>
 							<li>By: Gabriel Logan</li>
 						</ul>
 					</footer>
